136d18439b6d62908e8986069f907f9dd38d3f48
Adam Kozlowski <adamkozlowski0@gmail.com>
2021-02-27 16:19:45 +0000

    Day16

---
+++ src/main/java/Day17.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day17{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+
+        int x;
+        try {
+            x = Integer.parseInt(S);
+            System.out.println(x);
+        } catch (NumberFormatException e){
+            System.out.println("Bad String");
+        }
+    }
+}
