f75427e22a94b7d3e475f1fb9bd08d5acd91d91c
Adam Kozlowski <adamkozlowski0@gmail.com>
2021-02-28 20:54:24 +0000

    Day17

--- src/main/java/Day17.java
+++ src/main/java/Day17.java
@@ -1,17 +1,37 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Day17{
+class Calculator{
+
+    int power(int n, int p) throws IllegalArgumentException{
+
+        if(n < 0 || p < 0) {
+            throw new IllegalArgumentException("n and p should be non-negative");
+        }
+        else {
+            return (int) Math.pow(n, p);
+        }
+    }
+}
+
+class Day17{
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
 
-        int x;
-        try {
-            x = Integer.parseInt(S);
-            System.out.println(x);
-        } catch (NumberFormatException e){
-            System.out.println("Bad String");
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
+        in.close();
     }
 }
