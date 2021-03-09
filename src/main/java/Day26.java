bc03e895925e29da45b045128b332335c74e6dc9
Adam Kozlowski <adamkozlowski0@gmail.com>
2021-03-09 21:41:26 +0000

    Day26

--- src/main/java/Day26.java
+++ src/main/java/Day26.java
@@ -3,6 +3,8 @@ import java.time.YearMonth;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Day26 {
 
     public static LocalDate takeDate(int day, int month, int year) {
@@ -13,13 +15,16 @@ public class Day26 {
         int dayFine = 15;
         int monthFine = 500;
         int maxFine = 10000;
-        if (ret.getYear() > due.getYear()) {
+        if (ret.getYear() == due.getYear()) {
+            if (YearMonth.from(ret).isAfter(YearMonth.from(due))) {
+                int numberMonths = (int) ChronoUnit.MONTHS.between(due, ret);
+                return monthFine * numberMonths;
+            } else if (ret.getMonth() == due.getMonth() && ret.getDayOfMonth() > due.getDayOfMonth()) {
+                int numberDays = (int) ChronoUnit.DAYS.between(due, ret);
+                return numberDays * dayFine;
+            }
+        } else if (ret.getYear() > due.getYear()) {
             return maxFine;
-        } else if (ret.getYear() == due.getYear() && YearMonth.from(ret).isAfter(YearMonth.from(due))) {
-            int numberMonths = (int) ChronoUnit.MONTHS.between(due, ret);
-            return monthFine * numberMonths;
-        } else if (ret.getMonth() == due.getMonth() && ret.getYear() == due.getYear()) {
-            return (due.getDayOfMonth() - ret.getDayOfMonth()) * dayFine;
         }
         return 0;
     }
@@ -29,17 +34,19 @@ public class Day26 {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
-        int[] li = new int[3];
-
-        for (int i = 0; i < li.length; i++) {
-            li[i] = scanner.nextInt();
-        }
-        LocalDate returnDate = takeDate(li[0], li[1], li[2]);
-
-        for (int i = 0; i < li.length; i++) {
-            li[i] = scanner.nextInt();
-        }
-        LocalDate dueDate = takeDate(li[0], li[1], li[2]);
+//        int[] li = new int[3];
+//
+//        for (int i = 0; i < li.length; i++) {
+//            li[i] = scanner.nextInt();
+//        }
+//        LocalDate returnDate = takeDate(li[0], li[1], li[2]);
+//
+//        for (int i = 0; i < li.length; i++) {
+//            li[i] = scanner.nextInt();
+//        }
+//        LocalDate dueDate = takeDate(li[0], li[1], li[2]);
+        LocalDate returnDate = takeDate(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        LocalDate dueDate = takeDate(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
 
         System.out.println(fine(returnDate, dueDate));
     }
