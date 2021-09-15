63918d1dc163892e4b70db2e1664c276c4f4a773
AdamMK <adamkozlowski0@gmail.com>
2021-09-15 18:13:13 +0200

    Breaking the record

---
+++ src/main/kotlin/breakingTheRecord.kt
@@ -0,0 +1,27 @@
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+
+    var highBreak = 0
+    var lowBreak = 0
+    var firstScore = scores[0]
+
+    for (i in scores.indices) {
+        if (scores[i] > firstScore) {
+            highBreak++
+            firstScore = scores[i]
+        }
+    }
+
+    firstScore = scores[0]
+    for (i in scores.indices) {
+        if (scores[i] < firstScore) {
+            lowBreak++
+            firstScore = scores[i]
+        }
+    }
+    println("$highBreak, $lowBreak")
+    return arrayOf(highBreak, lowBreak)
+}
+
+fun main() {
+    print(breakingRecords(arrayOf(3,4,21,36, 10, 28, 35, 5, 24, 42)));
+}
