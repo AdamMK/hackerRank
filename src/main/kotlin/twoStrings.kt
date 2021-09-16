c1a38a9fc955c4f8b71650f7970ab310a61e69fb
AdamMK <adamkozlowski0@gmail.com>
2021-09-16 09:29:55 +0200

    Two Strings

---
+++ src/main/kotlin/twoStrings.kt
@@ -0,0 +1,16 @@
+fun twoStrings(s1: String, s2: String): String {
+
+    //not optimal for a very long strings
+    //if (s1.filter { it in s2 }.count() == 0) "NO" else "YES"
+
+    for ( c in "abcdefghijklmnopqrstuvwxyz") {
+        if (s1.indexOf(c)> -1 && s2.indexOf(c) > -1) {
+            return "YES"
+        }
+    }
+    return "NO"
+}
+
+fun main() {
+    print(twoStrings("aab", "bb"))
+}
