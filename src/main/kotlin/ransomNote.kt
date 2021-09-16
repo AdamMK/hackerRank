23bf82f1697b84a2cb266088c2cbb8ccd15af603
AdamMK <adamkozlowski0@gmail.com>
2021-09-16 14:49:59 +0200

    Ransom note

---
+++ src/main/kotlin/ransomNote.kt
@@ -0,0 +1,23 @@
+import java.util.function.BiFunction
+
+fun checkMagazine(magazine: Array<String>, note: Array<String>): Unit {
+
+    val magMap = mutableMapOf<String, Int>()
+    var check = true
+
+    for (word in magazine) {
+        magMap.compute(word) { _, v -> if (v == null) 1 else v + 1 }
+    }
+
+    for (word in note) {
+        if (!magMap.containsKey(word) || magMap[word] == 0) {
+            check = false
+        }
+        magMap.computeIfPresent(word) {_,v -> v-1}
+    }
+    if (check) print("Yes") else print("No")
+}
+
+fun main() {
+    checkMagazine(arrayOf("two", "times", "three", "is", "not", "four"), arrayOf("two", "times", "two", "is", "four"))
+}
